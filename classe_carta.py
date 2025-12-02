8b6228daf281d9886bd38c9e873ec8707625b0a9
VanessaCiani <vanessa.ciani08@gmail.com>
2025-12-02 16:56:33 +0100

    Update classe_carta.py

--- classe_carta.py
+++ classe_carta.py
@@ -1,8 +1,11 @@
 class Carta:
-    def __init__(self, seme: str, rango: str):
-        self.__seme = seme
-        self.__rango = rango
+    def __init__(self, seme: str, rango: str): #"__init__" --> COSTRUTTORE
+        #Rendo i due attributi privati tramite l'utilizzo di "__"
+        #Gli attributi privati possono essere modificati solo dall'interno della classe e non dall'esterno, questo passaggio serve a rendere i nostri dati più sicuri
+        self.__seme = seme       
+        self.__rango = rango     
 
+    #Imposto le property per il seme e per il rango
     @property
     def seme(self):
         return self.__seme
@@ -11,14 +14,20 @@ class Carta:
     def rango(self):
         return self.__rango
 
+    #Imposto la property per il valore
     @property
     def valore(self) -> int:
+        #Controllo che valore numerico associare ad ogni carta
         if self.__rango in ["J", "Q", "K"]:
-            return 10
+            return 10            
         elif self.__rango == "A":
-            return 11 
+            return 11            
         else:
-            return int(self.__rango)
+            return int(self.__rango)  
         
     def __str__(self):
+        #Rappresentazione testuale della carta
+        return f"Seme: {self.__seme}, Valore: {self.__rango}"
+
+
         return f"Seme: {self.__seme}, Valore: {self.__rango}"
