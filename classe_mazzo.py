04d6837ec99b251c8834101085e8657568f1e77c
VanessaCiani <vanessa.ciani08@gmail.com>
2025-12-02 17:01:24 +0100

    Add comments to Mazzo class methods and attributes
    Added comments to clarify the purpose of methods and attributes.

--- classe_mazzo.py
+++ classe_mazzo.py
@@ -2,15 +2,19 @@ import random
 
 class Mazzo:
     def __init__(self, lista_carte):
-        self.__carte = lista_carte
+        self.__carte = lista_carte  #Lista privata che contiene tutte le carte del mazzo
 
+    #Imposto la property
     @property
     def carte(self):
+        #
         return self.__carte
 
     def mescola(self):
+        # mescola le carte in modo casuale
         random.shuffle(self.__carte)
 
     def pesca(self):
+        # rimuove e restituisce l'ultima carta del mazzo
         carta_rimossa = self.__carte.pop()
         return carta_rimossa
